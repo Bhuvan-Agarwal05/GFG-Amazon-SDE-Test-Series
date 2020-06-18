06df14cf64e15b7adc9a0c17890e787ce9f95d31
Bhuvan-Agarwal <bhuvangarg123@gmail.com>
2020-06-18 11:35:38 +0530

    Third Largest Element Added

---
+++ Array/2_Third Largest Element/ThirdLargestElement.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+class ThirdLargestElement
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+			GfG g = new GfG();
+			System.out.println(g.thirdLargest(arr,n));
+			t--;
+		}
+	}
+}
+
+
+
+
+class GfG
+{
+    int thirdLargest(int a[], int n)
+    {
+	    // Your code here
+	    if(n<3)
+	      return -1;
+	    else
+	    {
+	        Arrays.sort(a);
+	        return a[n-3];
+	    }
+    }
+}
